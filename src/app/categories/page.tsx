086f9aa55bdc8bfbb0654839ed7a2ef375c008eb
wesleywil <wesleywilsonti@gmail.com>
2023-05-25 14:08:07 -0300

    updated categories page and category item component to handle the list of categories from the database call

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,7 +1,10 @@
+import { Category } from "@/utils/types";
 import Link from "next/link";
 import CategoryItem from "../components/category_item/category_item.component";
 
-export default function Categories() {
+export default async function Categories() {
+  const req = await fetch("http://localhost:3000/api/categories");
+  const categories: Category[] = await req.json();
   return (
     <main className="grow p-4 px-24 min-h-screen text-xl">
       <div className="mt-12 pb-4 flex justify-between text-4xl border-b-2">
@@ -17,11 +20,9 @@ export default function Categories() {
       <div className="pt-4">
         <h2>Categories</h2>
         <div className="mt-12 flex gap-4">
-          <CategoryItem />
-          <CategoryItem />
-          <CategoryItem />
-          <CategoryItem />
-          <CategoryItem />
+          {categories.map((item, index) => (
+            <CategoryItem key={index} category={item} />
+          ))}
         </div>
       </div>
     </main>
--- src/app/components/category_item/category_item.component.tsx
+++ src/app/components/category_item/category_item.component.tsx
@@ -1,8 +1,14 @@
-const CategoryItem = () => {
+import { Category } from "@/utils/types";
+
+type CategoryItemProps = {
+  category: Category;
+};
+
+const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
     <div className="flex w-48 h-24 border rounded-xl overflow-hidden">
       <div className="w-6 h-full flex items-center justify-center bg-yellow-500 ">
-        <h1 className=" -rotate-90">Travels</h1>
+        <h1 className=" -rotate-90">{category.name}</h1>
       </div>
       <div className="grow flex flex-col items-center justify-center">
         <div className="self-center p-2 bg-black text-white text-4xl rounded-full">
