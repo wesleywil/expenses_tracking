5334faeb127777e22f42bf13fce2e09750e6a1b9
wesleywil <wesleywilsonti@gmail.com>
2023-06-01 11:11:17 -0300

    updated types.ts by adding more interfaces

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,28 @@
+import { Decimal } from "@prisma/client/runtime";
+
+export interface User{
+    id:String,
+    name?:String,
+    email?:String,
+    image?:String,
+    expenses?:Expense[]
+}
+
+
 export interface Category {
     id:String,
     name:String,
     description:String,
+}
+
+export interface Expense{
+    id?:String,
+    vendor?:String,
+    created_at:Date,
+    description?:String,
+    status?:String,
+    amount?:Decimal,
+    user:User,
+    userId:String,
+    categories?: Category[],
 }
