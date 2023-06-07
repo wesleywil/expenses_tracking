597fc54599910884ac38bc1c9433bb70e7a39922
wesleywil <wesleywilsonti@gmail.com>
2023-06-07 17:25:28 -0300

    updated category_item component by including the count of expenses on each category

--- src/app/components/category_item/category_item.component.tsx
+++ src/app/components/category_item/category_item.component.tsx
@@ -1,18 +1,42 @@
+"use client";
+
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/redux/store";
+
 import { Category } from "@/utils/types";
+import { useEffect, useState } from "react";
 
 type CategoryItemProps = {
   category: Category;
 };
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
+  const expenses = useSelector((state: RootState) => state.expenses.expenses);
+  const [rcount, setRcount] = useState<number>(0);
+
+  useEffect(() => {
+    console.log("effect");
+    const categoryCount = expenses.reduce((count, expense) => {
+      const categoryNames = expense.categories!.map((cat) => cat.name);
+      const nameCount = categoryNames.filter(
+        (name) => name === category.name
+      ).length;
+      return count + nameCount;
+    }, 0);
+    setRcount(categoryCount);
+  }, [rcount]);
+
   return (
     <div className="flex w-48 h-24 border rounded-xl overflow-hidden">
-      <div className="w-6 h-full flex items-center justify-center bg-yellow-500 ">
-        <h1 className=" -rotate-90">{category.name}</h1>
+      <div
+        style={{ backgroundColor: category.color, color: category.textColor }}
+        className="w-6 h-full flex items-center justify-center"
+      >
+        <h1 className="-rotate-90">{category.name}</h1>
       </div>
       <div className="grow flex flex-col items-center justify-center">
         <div className="self-center p-2 bg-black text-white text-4xl rounded-full">
-          <h1>99</h1>
+          <h1>{rcount}</h1>
         </div>
       </div>
     </div>
