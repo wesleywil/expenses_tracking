abcdf263c57f146dbe3aa4392a3a53d3caf0043b
wesleywil <wesleywilsonti@gmail.com>
2023-06-16 15:53:31 -0300

    updated app by removing the function to create categories by the user

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -3,22 +3,18 @@ import Link from "next/link";
 import CategoryItem from "../../components/category_item/category_item.component";
 
 export default async function Categories() {
-  const req = await fetch("http://localhost:3000/api/categories");
+  const req = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-cache",
+  });
   const categories: Category[] = await req.json();
   return (
     <main className="grow p-4 px-24 min-h-screen text-xl text-[#fcfcfc]">
       <div className="mt-12 pb-4 flex justify-between text-4xl border-b-2">
         <h1>Categories</h1>
-        <Link
-          href="/new_category"
-          className="px-3 py-4 bg-[#26b3c4] text-2xl text-[#fcfcfc] rounded-xl"
-        >
-          New Category
-        </Link>
       </div>
       {/* Category list */}
       <div className="pt-4">
-        <h2>Categories</h2>
+        <h2>Categories(expenses count)</h2>
         <div className="mt-12 flex gap-4">
           {categories.map((item, index) => (
             <CategoryItem key={index} category={item} />
